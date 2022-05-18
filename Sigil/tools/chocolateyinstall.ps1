13cb0cc562f29043056e8cdbbbcde2d45ee740ba
yonizaf <yonizaf@gmail.com>
2022-05-18 13:00:45 +0300

    update sigil to 1.9.2, with fixes. - only use Legacy installer for Windows 7

--- Sigil/tools/chocolateyinstall.ps1
+++ Sigil/tools/chocolateyinstall.ps1
@@ -1,14 +1,29 @@
 ﻿$ErrorActionPreference = 'Stop';
 
+$url32          = 'https://github.com/Sigil-Ebook/Sigil/releases/download/1.9.2/Sigil-1.9.2-Windows-Setup.exe'
+$url64          = 'https://github.com/Sigil-Ebook/Sigil/releases/download/1.9.2/Sigil-1.9.2-Windows-x64-Setup.exe'
+$urlLegacy      = 'https://github.com/Sigil-Ebook/Sigil/releases/download/1.9.2/Sigil-1.9.2-Windows-Legacy-Setup.exe'
+$checksum       = '3763a3a004c64b2b18e5b25e5d604793d22c33ecc6830175751e41ad6cd6a296'
+$checksum64     = '50ee720dcfbed536502939aef7918d4f8192bc2e2458efda992de49d6129f543'
+$checksumLegacy = 'e6730a28f0a0d996316ab1597ac6fa26ce4edc8929aea9aae0b54de59f4277e6'
+
+if ([System.Environment]::OSVersion.Version -lt (new-object 'Version' 6, 2)) {
+	Write-Host "Installing Legacy version for Windows 7"; 
+	$url32 = $url64 = $urlLegacy
+	$checksum = $checksum64 = $checksumLegacy
+}
+
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
-  url64bit      = 'https://github.com/Sigil-Ebook/Sigil/releases/download/1.9.1/Sigil-1.9.1-Windows-Legacy-Setup.exe'
+  url           = $url32
+  url64bit      = $url64
 	
   softwareName  = 'sigil'
 
-  checksum64    = '4b6dea315da87ebc0cca1c8c584550bfb1f9b7772ee400234b39f4d43bb2cc36'
+  checksum    = $checksum
+  checksum64    = $checksum64
   checksumType64= 'sha256'
 
   silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
